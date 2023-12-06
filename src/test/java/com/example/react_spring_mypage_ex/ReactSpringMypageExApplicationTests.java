94a1e7fc14f28c19e5610e7d593eb9d03f52af31
LAYLALOAD <yjkim7885@knou.ac.kr>
2023-12-06 19:08:50 +0900

    first commit

---
+++ src/test/java/com/example/react_spring_mypage_ex/ReactSpringMypageExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_mypage_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringMypageExApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
